bdc1ec3af5f4f76316ba988aa668f3cc0212d721
Otavio Sartorelli de Toledo Piza <70035303+OtavioPiza@users.noreply.github.com>
2020-10-16 14:26:57 -0300

    Styling WIP!

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Button from './components/Button';
 
 function App() {
   return (
-    <div>
+    <div class='container'>
       <div class='logo-container'>
         <img src='./images/logo.png' alt='Algorithm Visualizer Logo'/>
       </div>
       <div class='text-container'>
         <strong>Algorithm Visualizer</strong>
         <p>An intuitive way to visualize how algorithms work</p>
+      </div>
+      <div class='button-container'>
         <Button text='About'/>
         <Button text='Explore'/>
       </div>
