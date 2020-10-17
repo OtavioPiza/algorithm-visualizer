c54279bf5012f700eae417e022e6dfa69d98cc15
Otavio Sartorelli de Toledo Piza <70035303+OtavioPiza@users.noreply.github.com>
2020-10-17 08:05:29 -0300

    Styling WIP!

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ eventHandler, text, src }) => (
     <button onClick={eventHandler}>{text}
-        <img src={src} alt={text}/>
+        <img src={src} alt=''></img>
     </button>
 )
 export default Button
