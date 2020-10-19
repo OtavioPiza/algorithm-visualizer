980157ab33b7c53788daa8f9cbca4a8954ed0fc7
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-10-19 15:59:22 -0300

    Updates

---
+++ precache-manifest.09a27a1c2705f63fb4202c527950a60b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1b357eebf01b457489466942d22734",
+    "url": "/algorithm-visualizer/index.html"
+  },
+  {
+    "revision": "97355b84262dc022a596",
+    "url": "/algorithm-visualizer/static/css/main.908b37e5.chunk.css"
+  },
+  {
+    "revision": "166ac71480dc92968ed4",
+    "url": "/algorithm-visualizer/static/js/2.27461d4b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/algorithm-visualizer/static/js/2.27461d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97355b84262dc022a596",
+    "url": "/algorithm-visualizer/static/js/main.6d882acf.chunk.js"
+  },
+  {
+    "revision": "7287ef28925d8f7d640a",
+    "url": "/algorithm-visualizer/static/js/runtime-main.a104d120.js"
+  },
+  {
+    "revision": "b135805cab4d40abd561a8d1d69a8a83",
+    "url": "/algorithm-visualizer/static/media/background.b135805c.svg"
+  },
+  {
+    "revision": "a818530540aa164372747f392d24f14e",
+    "url": "/algorithm-visualizer/static/media/bottom-bar.a8185305.svg"
+  },
+  {
+    "revision": "4dc7ff39e7640310425a0895c5f10f95",
+    "url": "/algorithm-visualizer/static/media/button-gray-3x1.4dc7ff39.svg"
+  },
+  {
+    "revision": "cfa501ba50c3556d5a650f03e232fd9b",
+    "url": "/algorithm-visualizer/static/media/button-pressed-orange-3x1.cfa501ba.svg"
+  },
+  {
+    "revision": "174d5ab67a890b00972c18bd5b490433",
+    "url": "/algorithm-visualizer/static/media/logo.174d5ab6.png"
+  },
+  {
+    "revision": "f508f9cf3b0c1b2d1a463317e739334d",
+    "url": "/algorithm-visualizer/static/media/thisisfine.f508f9cf.jpg"
+  }
+]);
