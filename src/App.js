83251532718c4205a2788dc505ba41e5f317643c
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-10-19 21:06:17 -0300

    WIP!

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './styles/App.css';
 import Button from './components/Button';
 import BottomBar from './components/BottomBar';
 import logo from './styles/images/logo.png'
+import Bar from './components/Bar';
 
 function App() {
 
@@ -10,16 +11,19 @@ function App() {
     <div className='container'>
       <div className='logo-container'>
         <img src={logo} alt='Algorithm Visualizer Logo'/>
+        <Bar size={ 200 }></Bar>
       </div>
+      <BottomBar/>
       <div className='text-container'>
         <strong>Algorithm Visualizer</strong>
         <p>An intuitive way to visualize how algorithms work</p>
       </div>
-      <BottomBar></BottomBar>
+      <BottomBar/>
       <div className='button-container'>
         <Button text='About'/>
         <Button text='Explore' id='thisisfine' eventHandler={() => console.log('this is fine')}/>
       </div>
+      <Bar size={1000}></Bar>
     </div>
   );
 }
