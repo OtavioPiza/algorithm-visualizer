a0bbd84762ec1e0ddf47f094c976901a46a13d17
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-10-20 18:59:03 -0300

    Switchable bars in the menu done!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react'
 import './styles/App.css';
 import Button from './components/Button';
 import BottomBar from './components/BottomBar';
 import Bar from './components/Bar';
 
-const bars = [
+const testlist = [
   {
     size: 450,
     selected: false
@@ -28,12 +28,47 @@ const bars = [
 ]
 
 function App() {
+  const [bars, setBars] = useState(testlist)
+  const [barsSelected, setBarsSelected] = useState([])
+
+  const selectBar = (id) => {
+    const bar = bars[id]
+    const changedBar = {...bar, selected: !bar.selected}
+    setBars(bars.map((bar, index) => index === id ? changedBar : bar))
+    if ( !bar.selected ) {
+      setBarsSelected(barsSelected.concat(id))
+    }
+  }
+  
+  if (barsSelected.length === 2) {
+    const firstBar = bars[barsSelected[0]]
+    const secondBar = bars[barsSelected[1]]
+
+    const newFirtBar = {...firstBar, size: secondBar.size, selected: false}
+    const newSecondBar = {...secondBar, size: firstBar.size, selected: false}
+
+    setBars(bars.map((bar, index) => {
+      switch (index) {
+        case barsSelected[0]:
+          return newFirtBar;
+      
+        case barsSelected[1]:
+          return newSecondBar;
+
+        default:
+          return bar;
+      }
+    }))
+
+    setBarsSelected([])
+  }
 
   return (
     <div className='container'>
       <div className='logo-container'>
         {bars.map((bar, index) => (
-          <Bar key={ index } id={ index } size={ bar.size } selected={ bar.selected }/>
+          <Bar key={ index } id={ index } size={ bar.size } selected={ bar.selected }
+          eventHandler={selectBar}/>
         ))}
       </div>
       <BottomBar/>
