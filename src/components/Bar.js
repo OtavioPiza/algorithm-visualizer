25883ed8f25e3a69ce2aca809c4136d4a5de8510
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-10-20 18:09:12 -0300

    Bar done!

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import '../styles/Bar.css'
+import selectedBar from '../styles/images/bar-selected-blue.svg'
 
-const Bar = ({ className, size }) => {
-    
+const Bar = ({ className, id, size, selected, eventHandler }) => {
+   
+    const background = selected ? `url(${selectedBar})` : ``
     const style = {
         width: "80px",
         height: size + "px",
+        backgroundImage: background,     
     }
     
     return (
-        <button className={ className === undefined ? 'Bar' : className } id={ size } style={style}
-        onClick={() => console.log('he')}>
+        <button className={ className === undefined ? 'Bar' : className } id={ size } style={ style }
+        onClick={() => eventHandler(id)}>
         </button>
     )
 }
