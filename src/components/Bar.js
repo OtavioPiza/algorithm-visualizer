63012ce80b4a70c5e34c771b958ca5b33492d0bc
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-10-22 20:00:08 -0300

    Added red bar!

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,24 +1,44 @@
 import React from 'react'
 import '../styles/components/Bar.css'
+import bar from '../styles/components/icons/bar-gray.svg'
 import selectedBar from '../styles/components/icons/bar-selected-blue.svg'
 import sortedBar from '../styles/components/icons/bar-sorted-green.svg'
-import pepe from "../styles/images/3.jpg"
+import unsortedBar from '../styles/components/icons/bar-unsorted-red.svg'
 
-const Bar = ({ className, id, size, selected, sorted, unsorted /** Delete */, eventHandler }) => {
+/**
+ * 
+ * @param { 
+ *  className: overwrites the default className
+ *  id: provides the id of the bar (use position in array)
+ *  size: sets the size of the bar
+ *  selected: informs wheter the bar is selected by the unsorted
+ *  sorted: informs wheter the bar is on a sorted array
+ *  unsorted: informs wheter the bar is on a reversed array
+ *  eventHandler: provides a handler for an onClick event
+ * } param0 
+ * 
+ * @returns a button that represents a bar in an array
+ * 
+ */
+const Bar = ({ className, id, size, selected, sorted, unsorted, eventHandler }) => {
    
-    const background = sorted ? `url(${sortedBar})` :
-        unsorted ? `url(${pepe})` : // Delete
-        selected ? `url(${selectedBar})` : ``
+    // Selects a background for the bar depending on its state
+    const background = 
+        sorted ? `url(${sortedBar})` :
+        unsorted ? `url(${unsortedBar})` :
+        selected ? `url(${selectedBar})` : 
+        `url(${bar})`
     
+    // Sets the on-screen bar size based on its properties
     const style = {
         width: "80px",
         height: size + "px",
-        backgroundImage: background,     
+        backgroundImage: background,
     }
-    
+
     return (
-        <button className={ className === undefined ? 'Bar' : className } id={ size } style={ style }
-        onClick={() => eventHandler(id)}>
+        <button className={ className === undefined ? 'Bar' : className } id={ id } 
+            style={ style } onClick={ () => eventHandler(id) }>
         </button>
     )
 }
