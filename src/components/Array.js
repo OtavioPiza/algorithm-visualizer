f3245ae62451f95ee40249d55876624987fbb4a9
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-10-22 23:51:16 -0300

    Array component

--- src/components/Array.js
+++ src/components/Array.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import Bar from './Bar'
 
+/**
+ * React component representing an array of Bar components
+ * 
+ * @param {
+ *  bars: array of bars objects
+ *  eventHandler: hadler for the onClick event
+ *  isSorted: identifies if the list sorted
+ *  isUnsorted: identifies if the list reversed
+ * } param0
+ * @returns an array containing a Bar component for each bar on the provided list
+ */
 const Array = ({ bars, eventHandler, isSorted, isUnsorted }) => {
 
     return (
