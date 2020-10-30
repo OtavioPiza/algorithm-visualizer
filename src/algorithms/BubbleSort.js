b6c8360ee70dfdec91decb2d2d0403378977bd4b
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-10-30 19:39:03 -0300

    Bubble sort WIP!

--- src/algorithms/BubbleSort.js
+++ src/algorithms/BubbleSort.js
@@ -1,5 +1,29 @@
-const BubbleSort = () => {
-    
+/**
+ * 
+ */
+const BubbleSort = ({ analyzedBarsIndex, greater, step, switched }, bars) => {
+
+    const compareBars = () => {
+        const newFirstBar = {...bars[analyzedBarsIndex[0]], selected: true}
+        const newSecondBar = {...bars[analyzedBarsIndex[1]], selected: true}
+
+        newBars = bars.map((bar, index) => {
+            switch (index) {
+                case analyzedBarsIndex[0]:
+                    return 
+
+                case analyzedBarsIndex[1]:
+
+
+            }
+        })
+    }
+
+    const switchBars = () => {
+
+    }
+
 }
 
 export default BubbleSort
+
