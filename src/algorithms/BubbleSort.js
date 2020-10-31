99d2f77090ed54bb4bfc81b829b9f9eea1f52fd4
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-10-30 21:30:32 -0300

    BubbleSort WIP!

--- src/algorithms/BubbleSort.js
+++ src/algorithms/BubbleSort.js
@@ -4,25 +4,35 @@
 const BubbleSort = ({ analyzedBarsIndex, greater, step, switched }, bars) => {
 
     const compareBars = () => {
-        const newFirstBar = {...bars[analyzedBarsIndex[0]], selected: true}
-        const newSecondBar = {...bars[analyzedBarsIndex[1]], selected: true}
-
-        newBars = bars.map((bar, index) => {
+        const newGreater = bars[analyzedBarsIndex[0]].size > bars[analyzedBarsIndex[1]].size
+        const newFirstBar = {...bars[analyzedBarsIndex[0]], analyzed: true}
+        const newSecondBar = {...bars[analyzedBarsIndex[1]], analyzed: true}
+        const newBars = bars.map((bar, index) => {
+            
             switch (index) {
                 case analyzedBarsIndex[0]:
-                    return 
+                    return newFirstBar
 
                 case analyzedBarsIndex[1]:
+                    return newSecondBar
 
-
+                default:
+                    return bar
             }
         })
+        return [
+            {
+                analyzedBarsIndex: analyzedBarsIndex,
+                greater: newGreater,
+                step: 1,
+                switched: false
+            },
+            newBars
+        ]
     }
-
     const switchBars = () => {
-
     }
-
+    return step === 0 ? compareBars() : switchBars()
 }
 
 export default BubbleSort
