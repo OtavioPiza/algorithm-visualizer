e596bfd8b1710c04345185eeb1c20dc7915293f9
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-11-01 22:16:31 -0300

    Decentralized arrays generation system

---
+++ src/services/ArrayManager.js
@@ -0,0 +1,40 @@
+let barList = [
+    {
+        size: 390,
+        selected: false,
+        analyzed: false
+    },
+    {
+        size: 350,
+        selected: false,
+        analyzed: false
+    },
+    {
+        size: 200,
+        selected: false,
+        analyzed: false
+    },
+    {
+        size: 280,
+        selected: false,
+        analyzed: false
+    },
+    {
+        size: 170,
+        selected: false,
+        analyzed: false
+    },
+    {
+        size: 410,
+        selected: false,
+        analyzed: false
+    },
+]
+
+const getBarList = () => barList
+
+const setBarList = (newList) => {
+    barList = newList
+}
+
+export default {getBarList, setBarList}
