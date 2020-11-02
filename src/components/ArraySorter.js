97dab48c955f5f413cc73800825129995e45e076
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-11-01 22:15:52 -0300

    Bugs fixed!

--- src/components/ArraySorter.js
+++ src/components/ArraySorter.js
@@ -1,14 +1,59 @@
 import React, { useState } from 'react'
-import Array from './Array'
+import Bar from './Bar'
 import BubbleSort from '../algorithms/BubbleSort'
 import Button from './Button'
+import isSorted from '../services/isSorted'
 
 const ArraySorter = ({ barList }) => {
     const [bars, setBars] = useState(barList)
+    const [barsSelected, setBarsSelected] = useState([])
+    const sorted = isSorted(bars)
+
+    const selectBar = (id) => {
+        const bar = bars[id]
+        const changedBar = {...bar, selected: !bar.selected}
+
+        setBars(bars.map((bar, index) => index === id ? changedBar : bar))
+
+        if (!bar.selected) {
+            setBarsSelected(barsSelected.concat(id))
+
+        } else {
+            setBarsSelected(barsSelected.filter(selected => selected !== id))
+        }
+    }
+
+    if (barsSelected.length === 2) {
+        const firstBar = bars[barsSelected[0]]
+        const secondBar = bars[barsSelected[1]]
+
+        const newFirstBar = {...firstBar, size: secondBar.size, selected: false}
+        const newSecondBar = {...secondBar, size: firstBar.size, selected: false}
+
+        setBars(bars.map((bar, index) => {
+            switch (index) {
+                case barsSelected[0]:
+                    return newFirstBar
+
+                case barsSelected[1]:
+                    return newSecondBar
+
+                default:
+                    return bar
+            }
+        }))
+        setBarsSelected([])
+    }
 
     return(
         <div className='ArraySorter'>
-            <Array barList={bars} simplified={true}/>
+            <div className='Array'>
+            {bars.map((bar, index) => (
+                <Bar key={ index } id={ index } size={ bar.size } analyzed={ bar.analyzed }
+                selected={ bar.selected } eventHandler={ selectBar } sorted={ sorted } 
+                simplified={ true }/>
+            ))}
+            </div>
             <Button text='click' eventHandler={() => setBars(BubbleSort({
                 analyzedBarsIndex: [0, 1],
                 greater: false,
