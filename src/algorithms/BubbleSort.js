744b5e0662121670d659be46979873e2b221f798
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-11-02 22:47:22 -0300

    Bubble sort core done!
    Needs refactoring

--- src/algorithms/BubbleSort.js
+++ src/algorithms/BubbleSort.js
@@ -1,39 +1,42 @@
-/**
- * 
- */
-const BubbleSort = ({ analyzedBarsIndex, greater, step, switched }, bars) => {
+import switchBars from '../services/switchBars'
+
+const BubbleSort = ({ analyzedBarsIndex, step }, bars) => {
 
     const compareBars = () => {
-        const newGreater = bars[analyzedBarsIndex[0]].size > bars[analyzedBarsIndex[1]].size
+        const endOfBar = analyzedBarsIndex[1] === bars.length - 1
+        const greater = bars[analyzedBarsIndex[0]].size > bars[analyzedBarsIndex[1]].size
         const newFirstBar = {...bars[analyzedBarsIndex[0]], analyzed: true}
         const newSecondBar = {...bars[analyzedBarsIndex[1]], analyzed: true}
-        const newBars = bars.map((bar, index) => {
-            
-            switch (index) {
-                case analyzedBarsIndex[0]:
-                    return newFirstBar
+        const newBars = bars.map((bar, index) => (
+            index === analyzedBarsIndex[0] ? newFirstBar :
+            index === analyzedBarsIndex[1] ? newSecondBar :
+            {...bar, analyzed: false}
+        ))
 
-                case analyzedBarsIndex[1]:
-                    return newSecondBar
-
-                default:
-                    return bar
-            }
-        })
-        return [
-            {
-                analyzedBarsIndex: analyzedBarsIndex,
-                greater: newGreater,
-                step: step + 1,
-                switched: false
+        console.log(analyzedBarsIndex[1] === bars.length - 1)
+        return [{
+            algorithmStatus: greater ? "Greater" : "Not greater",
+            analyzedBarsIndex: greater ? analyzedBarsIndex :
+                endOfBar ? [0, 1] : 
+                analyzedBarsIndex.map(index => index + 1),
+            step: greater ? 1 : 0,
+            switched: false
             },
             newBars
         ]
     }
-    const switchBars = () => {
-        
-    }
-    return step === 0 ? compareBars() : switchBars()
+
+    const changeBars = () => ([
+        {
+            algorithmStatus: '',
+            analyzedBarsIndex: analyzedBarsIndex.map(index => index + 1),
+            step: 0,
+            switched: true
+        },
+        switchBars(bars, analyzedBarsIndex[0], analyzedBarsIndex[1])
+    ])
+
+    return step === 0 ? compareBars() : changeBars()
 }
 
 export default BubbleSort
