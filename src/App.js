0e8507b1d09714a647fb8cf11047287c6edb81dd
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-11-17 17:18:14 -0300

    Finished routing

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './styles/App.css';
-import Button from './components/Button';
-import BottomBar from './components/BottomBar';
-import Array from './components/Array'
-import ArraySorter from './components/ArraySorter'
-import BubbleSort from './algorithms/bubbleSort'
-import arrayManager from './services/arrayManager';
+import BubbleSort from './BubbleSort'
+import Home from './Home';
 
 
 function App() {
 
   return (
-    <div className='container'>
-      <div className='logo-container'>
-        <Array barList={ arrayManager.getDefaultList() }/>
-      </div>
-      <BottomBar/>
-      <div className='text-container'>
-        <strong>Algorithm Visualizer</strong>
-        <p>An intuitive way to visualize how algorithms work</p>
-      </div>
-      <BottomBar/>
-      <div className='button-container'>
-        <Button text='About'/>
-        <Button text='Explore' id='thisisfine' eventHandler={() => console.log('this is fine')}/>
-      </div>
-      <BottomBar/>
-      <ArraySorter barList={ arrayManager.getRandomList(10) } sortingAlgorithm={ BubbleSort }/>
-      
+    <Router>
 
-    </div>
+      <div>
+        <Link to="/">home</Link>
+        <Link to="/bubblesort">bbsort</Link>
+      </div>
+
+      <Switch>
+
+        <Route path="/bubblesort" component={BubbleSort} />
+        <Route path="/" component={Home} />
+
+      </Switch>
+
+    </Router>
   );
 }
 
