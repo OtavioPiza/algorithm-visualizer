481b457452501c660b78d50e21fdb729d3e49cb3
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-11-17 18:45:18 -0300

    Improved css to accommodate new cool red button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import grayButton from '../styles/components/icons/button-gray-3x1.svg'
+import redButton from '../styles/components/icons/button-red-3x1.svg'
 import '../styles/components/Button.css'
 
-const Button = ({ eventHandler, text, id, className }) => (
-    <button id={id} className={ className === undefined ? 'Button' : className} 
-    onClick={eventHandler}>
-        <span>
-            {text}
-        </span>
-    </button>
-)
+const Button = ({ eventHandler, text, id, className, red = false }) => {
+
+    const background = red ? redButton : grayButton
+
+    const style = {
+        background: `url(${background})`,
+        backgroundSize: "100% 100%",
+        backgroundRepeat: "no-repeat",
+        color: `${red ? "#0F0D0D" : "#F4F4F4"}`
+    }
+
+    return (
+        <button id={id} className={className === undefined ? 'Button' : className}
+            onClick={eventHandler} style={style}>
+            <span>
+                {text}
+            </span>
+        </button>
+    )
+}
 
 export default Button
