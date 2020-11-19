bcd59c9ae47d62a76f826209740a44ba422c7416
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-11-18 21:14:14 -0300

    Refactoring

--- src/BubbleSort.js
+++ src/BubbleSort.js
@@ -1,18 +1,13 @@
 import React from 'react'
-import './styles/App.css';
 import bubbleSort from './algorithms/bubbleSort'
 import arrayManager from './services/arrayManager';
 import ArraySorter from './components/ArraySorter'
-import Header from './components/Header'
 
 
 const BubbleSort = () => {
 
   return (
-    <div className='container'>
-        <Header title={"BubbleSort"} link={"/"} />
-        <ArraySorter barList={ arrayManager.getRandomList(10) } sortingAlgorithm={ bubbleSort }/>
-    </div>
+    <ArraySorter barList={arrayManager.getRandomList(10)} sortingAlgorithm={bubbleSort} />
   );
 }
 
