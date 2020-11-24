e40740639c6bf428339ba8a277956063de5bcc4a
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-11-24 17:49:25 -0300

    Header done!

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,18 @@ import React from 'react'
 import "../styles/components/Header.css"
 import { Link } from 'react-router-dom'
 
-const Header = ({link='/', title}) => {
-
-    return (
-        <div className="Header">
-            <h1>
-                {title}
-            </h1>
-            <Link to={link}>Go back</Link>
-        </div>
-    )
-}
+/**
+ * React component representing a header
+ * 
+ * @param {String, String} param0 link, title
+ */
+const Header = ({ link = '/', title }) => (
+    <div className="Header">
+        <h1>
+            {title}
+        </h1>
+        <Link to={link}>Go back</Link>
+    </div>
+)
 
 export default Header
