8700079c8f07712126fbc6763594c7aec6601eb0
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-11-23 23:56:30 -0300

    Fixed css!

--- src/Home.js
+++ src/Home.js
@@ -9,19 +9,21 @@ import './styles/Home.css';
 const Home = () => {
 
   return (
-    <div className='Home'>
-      <div className='logo-container'>
-        <Array barList={ arrayManager.getDefaultList() }/>
-      </div>
-      <BottomBar/>
-      <div className='text-container'>
-        <strong>Algorithm Visualizer</strong>
-        <p>An intuitive way to visualize how algorithms work</p>
-      </div>
-      <BottomBar/>
-      <div className='button-container'>
-        <RedirectButton text='About'/>
-        <RedirectButton text='Explore' id='thisisfine' href={"/bubblesort"}/>
+    <div className='Container'>
+      <div className='Home'>
+        <div className='logo-container'>
+          <Array barList={arrayManager.getDefaultList()} />
+        </div>
+        <BottomBar />
+        <div className='text-container'>
+          <strong>Algorithm Visualizer</strong>
+          <p>An intuitive way to visualize how algorithms work</p>
+        </div>
+        <BottomBar />
+        <div className='button-container'>
+          <RedirectButton text='About' />
+          <RedirectButton text='Explore' id='thisisfine' href={"/bubblesort"} />
+        </div>
       </div>
     </div>
   );
