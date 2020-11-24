7352b974d78f7b742bfd225bac87497ce4870809
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-11-24 17:55:29 -0300

    isSorted done!

--- src/services/isSorted.js
+++ src/services/isSorted.js
@@ -1,9 +1,14 @@
+/**
+ * Returns if an array of bars is sorted or not
+ * 
+ * @param {Array} list array of bars
+ */
 const isSorted = (list) => {
     let sorted = 1;
 
     for (let i = 1; i < list.length; i++) {
 
-    if (list[i].size < list[i - 1].size) {
+        if (list[i].size < list[i - 1].size) {
             sorted = 0
             break
         }
