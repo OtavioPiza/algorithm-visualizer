d83691b5273fbb23f71ac519b47d4e34f602ee73
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-11-24 17:44:05 -0300

    BottomBar done!

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import '../styles/components/BottomBar.css'
 
+/**
+ * React component representing a BottomBar
+ * 
+ * @param {String} param class name
+ * 
+ * @returns {BottomBar} BottomBar
+ */
 const BottomBar = ({ className }) => (
     <footer className={ className === undefined ? 'BottomBar' : className }/>
 )
