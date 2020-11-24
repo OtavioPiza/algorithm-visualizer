1e43497fb6669b3958ba7ba44cd2bb924825d7b6
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-11-24 17:46:26 -0300

    Button done!

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,16 +3,29 @@ import grayButton from '../styles/components/icons/button-gray-3x1.svg'
 import redButton from '../styles/components/icons/button-red-3x1.svg'
 import '../styles/components/Button.css'
 
+/**
+ * React component representing a button
+ * 
+ * @param {{Function, String, Integer, String, Boolean}} param 
+ */
 const Button = ({ eventHandler, text, id, className, red = false }) => {
 
+    /**
+     * Sets the button background depending on its state
+     */
     const background = red ? redButton : grayButton
 
+    /**
+     * Sets the button style dependign on its state
+     */
     const style = {
         background: `url(${background})`,
         backgroundSize: "100% 100%",
         backgroundRepeat: "no-repeat",
         color: `${red ? "#0F0D0D" : "#F4F4F4"}`
     }
+
+    // == HTML ====================================================================================================== //
 
     return (
         <button id={id} className={className === undefined ? 'Button' : className}
