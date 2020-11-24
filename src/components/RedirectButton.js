8f74d0a1aceb77d98ad9b068e10ee409423add4d
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-11-24 17:51:07 -0300

    RedirectButton done!

--- src/components/RedirectButton.js
+++ src/components/RedirectButton.js
@@ -2,6 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/components/RedirectButton.css'
 
+/**
+ * React component representing a redirect button
+ * 
+ * @param {String, String, Integer, String} param0 link, text, id, classname
+ */
 const RedirectButton = ({ href, text, id, className }) => (
     <Link id={id} className={className === undefined ? 'RedirectButton' : className} to={href}>
 
