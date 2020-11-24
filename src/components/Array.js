a7c3e8c5ddc23e6d87f735c464a3790735711de4
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-11-24 17:26:18 -0300

    Array done!

--- src/components/Array.js
+++ src/components/Array.js
@@ -13,9 +13,17 @@ import '../styles/components/Array.css'
  * @returns {Array} : an array containing a Bar component for each bar on the provided list
  */
 const Array = ({ barList, simplified }) => {
+
+    /* Holds an array of bars */
     const [bars, setBars] = useState(barList)
+
+    /* Holds the index of the selected bars */
     const [barsSelected, setBarsSelected] = useState([])
+
+    /* Holds whether the array is sorted */
     const sorted = isSorted(bars)
+
+    // == User Interactivity ======================================================================================== //
 
     /**
      * Selects a Bar in the provided index
@@ -24,7 +32,7 @@ const Array = ({ barList, simplified }) => {
      */
     const selectBar = (id) => {
         const bar = bars[id]
-        const changedBar = {...bar, selected: !bar.selected}
+        const changedBar = { ...bar, selected: !bar.selected }
 
         setBars(bars.map((bar, index) => index === id ? changedBar : bar))
 
@@ -34,41 +42,43 @@ const Array = ({ barList, simplified }) => {
         } else {
             setBarsSelected(barsSelected.filter(selected => selected !== id))
         }
+
+        /**
+         * Switches two selected bars
+         */
+        if (barsSelected.length === 2) {
+            const firstBar = bars[barsSelected[0]]
+            const secondBar = bars[barsSelected[1]]
+
+            const newFirstBar = { ...firstBar, size: secondBar.size, selected: false }
+            const newSecondBar = { ...secondBar, size: firstBar.size, selected: false }
+
+            setBars(bars.map((bar, index) => {
+                switch (index) {
+                    case barsSelected[0]:
+                        return newFirstBar
+
+                    case barsSelected[1]:
+                        return newSecondBar
+
+                    default:
+                        return bar
+                }
+            }))
+            setBarsSelected([])
+        }
     }
 
-    /**
-     * Switches two selected bars
-     */
-    if (barsSelected.length === 2) {
-        const firstBar = bars[barsSelected[0]]
-        const secondBar = bars[barsSelected[1]]
-
-        const newFirstBar = {...firstBar, size: secondBar.size, selected: false}
-        const newSecondBar = {...secondBar, size: firstBar.size, selected: false}
-
-        setBars(bars.map((bar, index) => {
-            switch (index) {
-                case barsSelected[0]:
-                    return newFirstBar
-
-                case barsSelected[1]:
-                    return newSecondBar
-
-                default:
-                    return bar
-            }
-        }))
-        setBarsSelected([])
-    }
+    // == HTML ====================================================================================================== //
 
     return (
-       <div className='Array'>
+        <div className='Array'>
             {bars.map((bar, index) => (
-                <Bar key={ index } id={ index } size={ bar.size } analyzed={ bar.analyzed }
-                selected={ bar.selected } eventHandler={ selectBar } sorted={ sorted } 
-                simplified={ simplified }/>
+                <Bar key={index} id={index} size={bar.size} analyzed={bar.analyzed}
+                    selected={bar.selected} eventHandler={selectBar} sorted={sorted}
+                    simplified={simplified} />
             ))}
-       </div>
+        </div>
     )
 }
 
