87da318a48838b8ae7c752eff15c0f2458d7785c
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-12-16 18:05:43 -0300

    started insertion sort

---
+++ src/algorithms/sorting/insertionSort.js
@@ -0,0 +1,126 @@
+import React from 'react'
+import arrayManager from '../../services/arrayManager'
+
+/**
+ * Returns the state of the algorithm
+ * 
+ * @param {Bar[]} barArray              : an array with the bars to be sorted
+ * @param {Boolean} isSorted            : if the array is sorted or not   
+ * @param {Number} currentComplexity    : current complexity of the algorithm
+ */
+const defaultState = (barArray, isSorted = false, currentComplexity) => [
+    {
+        algorithmStatus: isSorted ? "Finished sorting!" : "Ready to start sorting!",
+        analyzedBarsIndex: [-1, 0],
+        upperbound: barArray.length - 1,
+        lowerbound: 0,
+        sorted: isSorted,
+        switched: false,
+        step: 0,
+        worseComplexity: (barArray.length * (barArray.length - 1)) / 2 + 1,
+        bestComplexity: barArray.length - 1,
+        currentComplexity: currentComplexity === undefined ? 0 : currentComplexity,
+    },
+    barArray.map(bar => ({ ...bar, analyzed: false, sorted: isSorted })),
+]
+
+/**
+ * Takes one step in the sorting algorithm
+ * 
+ * @param {State} state : state of the algorithm
+ * 
+ * @returns {State}     : new state of the algorithm
+ */
+const sort = (state) => {
+    const status = state[0]
+    const bars = state[1]
+
+    /**
+     * Returns the next two bars that will be analyzed by te algorithm
+     */
+    const getNextBars = () => (
+        status.analyzedBarsIndex[0] >= status.upperbound ? [0, 1] :
+            status.analyzedBarsIndex.map(bar => bar + 1)
+    )
+
+    /**
+     * Determines if the array is sorted
+     */
+    const getIsSorted = () => {
+        
+    }
+
+    /**
+     * Compares two bars
+     */
+    const compareBars = () => {
+        
+    }
+
+    /**
+     * Changes the two bars and updates the status
+     */
+    const changeBars = () => {
+
+    }
+}
+
+/**
+ * Returns the name of the algorithm
+ * 
+ * @returns {String}    : name of the sorting algorithm
+ */
+const name = () => 'Insertion Sort'
+
+/**
+ * Returns the about section of the algorithm
+ * 
+ * @returns {String}    : about section of the algorithm
+ */
+const about = () => (
+
+    <div className="InsertionSortAbout">
+
+        <h3>
+            About Insertion Sort
+        </h3>
+
+        <p>
+            TODO
+        </p>
+
+        <h3>
+            How our Implementation Works
+        </h3>
+
+        <ul>
+            <li><strong>Blue: </strong>bar is selected by the user</li>
+            <li><strong>Orange: </strong>bar is currently analyzed</li>
+            <li><strong>Gray: </strong>bar is considered sorted by the algorithm</li>
+            <li><strong>Green: </strong>the list is sorted</li>
+        </ul>
+
+    </div>
+)
+
+/**
+ * Returns a python implementation of the algorithm
+ * 
+ * @returns {HTML}  : returns an implementation of the algorithm
+ */
+const implementation = () => (
+    <div className="BubbleSortImplementation">
+
+        <h3>
+            Python Implementation
+        </h3>
+
+        <pre>
+            <code>
+                {`TODO`}
+            </code>
+        </pre>
+    </div>
+)
+
+export default { sort, defaultState, name, implementation, about }
