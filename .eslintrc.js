441b9c9b62a3e55d19921f3abe860f63916ffc16
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-01-15 21:17:32 -0300

    wip!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,45 @@
 module.exports = {
-  env: {
-    'commonjs': true,
-    'es6': true,
-    'node': true
-  },
-  parser: 'babel-eslint',
-  parserOptions: {
-    allowImportExportEverywhere: true
-  },
-  rules: {
-    indent: [
-      'error',
-      2
-    ],
-    quotes: [
-      'error',
-      'single'
-    ],
-    semi: [
-      'error',
-      'never'
-    ]
-  }
+    'env': {
+        'commonjs': true,
+        'es6': true,
+        'node': true,
+        'jest': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'allowImportExportEverywhere': true,
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ],
+        'no-console': 0
+    }
 }
