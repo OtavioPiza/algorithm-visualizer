40282cf17c73dbf8af768d21015ff94fc2c7d037
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-07-20 14:07:46 -0300

    removed debugging statements

--- src/util/arrayManager.js
+++ src/util/arrayManager.js
@@ -122,7 +122,6 @@ const selectBar = (index, bars) => {
 
     /* the bar was already selected */
     if (bars[index].selected) {
-        console.log('cancel');
         return bars.map((bar) => ({ ...bar, status: 0 }));
     }
 
@@ -130,7 +129,6 @@ const selectBar = (index, bars) => {
 
     /* searches for another selected bar */
     for (let i = 0; i < bars.length; i++) {
-        console.log('try');
 
         if (bars[i].status === 1) {
             return switchBars(bars, index, i).map((bar) => ({ ...bar, status: bar.status === 1 ? 0 : bar.status }))
